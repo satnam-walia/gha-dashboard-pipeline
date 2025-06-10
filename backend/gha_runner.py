23b3e346481006edd9e503727d598424588839e8
Aimen Guemmat <guemmataimen@gmail.com>
2025-06-10 18:55:15 -0400

    Make wrapper runnable without REST
    - Add code to execute wrapper from terminal
    - To test:
      - set your github token as the value "token" variable in gha_runner.py
    file
      - cd into root of project
      - execute "pipenv shell"
      - execute "python backend/gha_runner.py"

--- backend/gha_runner.py
+++ backend/gha_runner.py
@@ -13,6 +13,9 @@ GHAMETRICS_PATH = GHAMINER_PATH / "src" / "GHAMetrics.py"
 STATE_FILE = BASE_DIR / "output" / "state.json"
 OUTPUT_DIR = BASE_DIR / "output" / "raw"
 
+repo_url = "https://github.com/alchemy-fr/Phraseanet"
+token = ""
+
 
 def clone_or_update_ghaminer():
     repo_url = "https://github.com/stilab-ets/GHAminer.git"
@@ -26,14 +29,16 @@ def clone_or_update_ghaminer():
         print("[!] GHAMiner folder exists but is incomplete. Removing and recloning...")
         shutil.rmtree(GHAMINER_PATH)
 
-    subprocess.run(["git", "clone", repo_url, str(GHAMINER_PATH)], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    subprocess.run(["git", "clone", repo_url, str(GHAMINER_PATH)],
+                   check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     return
 
 
 def should_run():
     if STATE_FILE.exists():
         with open(STATE_FILE) as f:
-            last_run = datetime.fromisoformat(json.load(f).get("last_run", "1970-01-01T00:00:00"))
+            last_run = datetime.fromisoformat(
+                json.load(f).get("last_run", "1970-01-01T00:00:00"))
         if datetime.utcnow() - last_run < timedelta(minutes=5):
             print("[ℹ] GHAMiner run is up-to-date.")
             return False
@@ -54,13 +59,29 @@ def run_ghaminer_if_needed(repo_url: str, token: str):
 
     repo_name = repo_url.split("/")[-1].replace(".git", "")
     print(f"[→] Running GHAMiner on {repo_name}...")
-    subprocess.run([
-        sys.executable, str(GHAMETRICS_PATH),
-        "--s", repo_url,
-        "--token", token
-    ], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    # ------
+    # subprocess.run([
+    #     sys.executable, str(GHAMETRICS_PATH),
+    #     "--s", repo_url,
+    #     "--token", token
+    # ], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    # ------
+    try:
+        subprocess.run([sys.executable, str(GHAMETRICS_PATH),
+                        "--s", repo_url,
+                        "--token", token],
+                       check=True, capture_output=True, text=True)
+    except subprocess.CalledProcessError as e:
+        print("ERROR!! GHAMiner")
+        print("STDOUT: ", e.stdout)
+        print("STDERR: ", e.stderr)
+
     print(f"[✔] Completed {repo_name}")
 
     STATE_FILE.parent.mkdir(parents=True, exist_ok=True)
     with open(STATE_FILE, "w") as f:
         json.dump({"last_run": datetime.utcnow().isoformat()}, f)
+
+
+if __name__ == "__main__":
+    run_ghaminer_if_needed(repo_url,token)
